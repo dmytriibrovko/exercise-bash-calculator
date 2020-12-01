888088b38bcd3868a3b5bce0a946e1feea7eda36
Ubuntu <ubuntu@ip-172-31-44-110.eu-central-1.compute.internal>
2020-12-01 15:21:26 +0000

    fix division

--- division.sh
+++ division.sh
@@ -15,19 +15,20 @@ if [[ $1  == *"."* ]]; then
          if [[ $first_num_dat -ge $second_num_dat ]]; then
             first_num_after=$((($first_num_dat_first*10**($first_num_dat))+$first_num_dat_second))
             second_num_after=$((($second_num_dat_first*10**($first_num_dat))+$second_num_dat_second))
-            result=$(($first_num_after/$second_num_after))
-            echo $result
+            printf %.3f "$((10**3 * $first_num_after/$second_num_after))e-3"
+            echo " "
             exit 0
          else
             first_num_after=$((($first_num_dat_first*10**($second_num_dat))+$first_num_dat_second))
             second_num_after=$((($second_num_dat_first*10**($second_num_dat))+$second_num_dat_second))
-            result=$(($first_num_after/$second_num_after))
-            echo $result
+            printf %.3f "$((10**3 * $first_num_after/$second_num_after))e-3"
+            echo " "
             exit 0
          fi
       else
-         result=$(($first_num_after/($second_num*10**($first_num_dat))))
-         echo $result
+         second_num_comp=$(($second_num*10**$first_num_dat))
+         printf %.3f "$((10**3 * $first_num_after/$second_num_comp))e-3"
+         echo " "
          exit 0
       fi
    else
@@ -35,14 +36,13 @@ if [[ $1  == *"."* ]]; then
       second_num_dat_second=`echo "$second_num" |cut -f2 -d.`
       second_num_dat=`echo -n $second_num_dat_second |wc -c`
       second_num_after=$((($second_num_dat_first*10**($second_num_dat))+$second_num_dat_second))
-      first_num_dat_second=`echo "$first_num" |cut -f2 -d.`
-      first_num_dat=`echo -n $first_num_dat_second |wc -c`
-      result=$(($second_num_after/($first_num*10**($first_num_dat))))
-      echo $result
+      first_num_comp=$(($first_num*10**$second_num_dat))
+      printf %.3f "$((10**3 * $first_num_comp/$second_num_after))e-3"
+      echo " "
       exit 0
    fi
 else
-   printf %.5f "$((10**5 * $1))e-5"
+   printf %.3f "$((10**3 * $1))e-3"
    echo " "
    exit 0
 fi
