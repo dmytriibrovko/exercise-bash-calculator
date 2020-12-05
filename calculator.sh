3f83426ee8db2dfe295549e48adfdd3627826a97
Ubuntu <ubuntu@ip-172-31-44-110.eu-central-1.compute.internal>
2020-12-05 23:53:10 +0000

    add input check

--- calculator.sh
+++ calculator.sh
@@ -1,24 +1,28 @@
+
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
     echo "No arguments"
     exit 123
-elif [[ $1 == *[A-Za-z]* ]]; then
-    echo "Use only numbers and basic arithmetic"
-    exit 123
-elif [[ $1 == *"+"* ]]; then
-     source  ./plus.sh
-     plus $1
-elif [[ $1 == *"-"* ]]; then
-     source  ./minus.sh
-     minus $1
-elif [[ $1 == *"*"* ]]; then
-     source  ./multiply.sh
-     multiply $1
-elif [[ $1 == *"/"* ]]; then
-     source  ./division.sh
-     division $1
+elif [[ $1 =~ (^[0-9]*[.]?[0-9]+)([-+\/\*])([0-9]*[.]?[0-9]+$) ]]; then
+     if [[ $1 =~ (^[0-9]*[.]?[0-9]+)([+])([0-9]*[.]?[0-9]+$) ]]; then
+       source  ./plus.sh
+       plus $1
+     elif [[ $1 =~ (^[0-9]*[.]?[0-9]+)([-])([0-9]*[.]?[0-9]+$) ]]; then
+       source  ./minus.sh
+       minus $1
+     elif [[ $1 =~ (^[0-9]*[.]?[0-9]+)([*])([0-9]*[.]?[0-9]+$) ]]; then
+       source  ./multiply.sh
+       multiply $1
+     elif [[ $1 =~ (^[0-9]*[.]?[0-9]+)([/])([0-9]*[.]?[0-9]+$) ]]; then
+       source  ./division.sh
+       division $1
+     else
+       echo "Please double-check the equation"
+       exit 123
+     fi
 else
-    echo "Use basic arithmetic operations such as : - + * /"
+    echo "Use only numbers and basic arithmetic operations such as : - + * /."
+    echo "The calculator calculates only linear single-complex equation"
     exit 123
 fi
